16b62945674950d2ff2eeb07fc933aec296cf057
Carson Trainer <carson.trainer@outlook.com>
2023-07-18 17:25:24 -0700

    added layout for posting page

--- src/pages/CreatePost.tsx
+++ src/pages/CreatePost.tsx
@@ -1,24 +1,119 @@
-import { Button, Typography, AppBar, Alert, AlertTitle, Box, Container } from "@mui/material";
+import {
+  Paper,
+  Typography,
+  Box,
+  Grid,
+  TextField,
+  Button,
+  InputAdornment,
+  MenuItem,
+} from "@mui/material";
 import Navigation from "./components/Navigation";
 
-export default function CreatePost() {
+const conditions = [
+  {
+    value: "1",
+    label: "Excellent",
+  },
+  {
+    value: "2",
+    label: "Good",
+  },
+  {
+    value: "3",
+    label: "Fair",
+  },
+];
 
-    return (
-        <>
-        <Navigation />
-        <Container>
-            <Box sx={{ mt: 15 }}>
-                <Typography variant="h2">
-                    Create a Post
-                </Typography>
-            </Box>
-            <Box sx={{ mt: 5 }}>
-                <Alert severity="warning">
-                    <AlertTitle>Warning</AlertTitle>
-                        This site is <strong>under construction.</strong>
-                </Alert>
-            </Box>
-        </Container>
-        </>
-    );
+export default function CreatePost() {
+  return (
+    <>
+      <Navigation />
+      <Box sx={{ justifyContent: "center" }}>
+        <Typography variant="h3" sx={{ mt: 14, ml: 20 }}>
+          Post your Surfboard
+        </Typography>
+        <Grid
+          container
+          spacing={3}
+          justifyContent="center"
+          sx={{ mt: 1, mb: 10 }}
+        >
+          <Paper sx={{ p: 3 }} elevation={4}>
+            <Typography variant="h5" sx={{ mb: 3 }}>
+              Please enter the following details:
+            </Typography>
+            <TextField
+              id="outlined-standard-basic"
+              label="Model"
+              helperText="Surfboard Model/Name"
+              variant="outlined"
+              fullWidth
+              required
+              sx={{
+                mb: 3,
+              }}
+            />
+            <TextField
+              id="outlined-standard-basic"
+              label="Size"
+              variant="outlined"
+              fullWidth
+              helperText="Please use factory measurements"
+              required
+              sx={{
+                mb: 3,
+              }}
+            />
+            <TextField
+              id="outlined-standard-basic"
+              select
+              label="Condition"
+              variant="outlined"
+              fullWidth
+              required
+              sx={{
+                mb: 3,
+              }}
+            >
+              {conditions.map((option) => (
+                <MenuItem key={option.value} value={option.value}>
+                  {option.label}
+                </MenuItem>
+              ))}
+            </TextField>
+            <TextField
+              id="standard-basic"
+              label="Hourly Price"
+              variant="outlined"
+              fullWidth
+              InputProps={{
+                startAdornment: (
+                  <InputAdornment position="start">$</InputAdornment>
+                ),
+              }}
+              required
+              sx={{
+                mb: 3,
+              }}
+            />
+            <TextField
+              id="outlined-standard-basic"
+              label="Description"
+              variant="outlined"
+              fullWidth
+              helperText="Prefered usage, rider size, etc."
+              required
+              sx={{
+                mb: 3,
+              }}
+            />
+            <Button href="#" variant="outlined">
+              Submit
+            </Button>
+          </Paper>
+        </Grid>
+      </Box>
+    </>
+  );
 }
