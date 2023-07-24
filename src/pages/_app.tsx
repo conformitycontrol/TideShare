4ddc1dfea8c60bc64e53800514492cc181ca621e
Carson Trainer <carson.trainer@outlook.com>
2023-07-24 14:51:49 -0700

    some color ideas

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,12 +1,28 @@
 import { api } from "~/utils/api";
 import { ClerkProvider } from "@clerk/nextjs";
 import type { AppProps } from "next/app";
- 
+import { createTheme } from "@mui/material"; 
+import { ThemeProvider } from "@emotion/react";
+
+const theme = createTheme({
+  palette: {
+    background: {
+      default: "#f5eed5"
+    },
+    primary: {
+      main: "#f5eed5"
+    }
+  }
+})
+
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ClerkProvider {...pageProps}>
-      <Component {...pageProps} />
-    </ClerkProvider>
+    <ThemeProvider theme={theme}>
+      <ClerkProvider {...pageProps}>
+        <Component {...pageProps} />
+      </ClerkProvider>
+    </ThemeProvider>
+
   );
 }
  
