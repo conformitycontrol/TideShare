d568a27f34ead4f5ca8139a43cd380921d836ecb
Carson Trainer <carson.trainer@outlook.com>
2023-07-29 18:48:08 -0700

    bugfixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,16 +1,6 @@
 import { alignProperty } from "@mui/material/styles/cssUtils";
 import Navigation from "./components/Navigation";
-import {
-  Typography,
-  Alert,
-  AlertTitle,
-  Box,
-  Container,
-  Link,
-  Button,
-} from "@mui/material";
-import surfpic1 from "../../public/about_us_pic1.jpg";
-import Image from "next/image";
+import { Typography, Box, Container, Link, Button } from "@mui/material";
 
 export default function AboutUs() {
   return (
@@ -22,20 +12,25 @@ export default function AboutUs() {
             SURF LESS
           </Typography>
           <Typography variant="h2"> + OWN LESS.</Typography>
-          <Box sx={{ width: "450px", mb: 3 }}>
+          <Box component="div" sx={{ width: "450px", mb: 3 }}>
             <Typography variant="h5">
               Your ultimate surfboard rental destination for endless ocean
               adventures.
             </Typography>
-
-            <Link href="#">
-              <Button variant="contained" sx={{ mt: 2 }}>
-                Contact us
-              </Button>
-              <Button variant="contained" href="./AllPosts" sx={{ mt: 2, ml: 2 }}>
+            <Button component="div" variant="contained" href="#" sx={{ mt: 2 }}>
+              Contact us
+            </Button>
+            <Button
+              component="div"
+              variant="contained"
+              sx={{ mt: 2, ml: 2 }}
+            >
+              <Link href="./AllPosts" sx={{ color: "#ffffff"}} underline="none">
                 Boards
-              </Button>
-            </Link>
+              </Link>
+              
+
+            </Button>
           </Box>
         </Box>
       </Container>
